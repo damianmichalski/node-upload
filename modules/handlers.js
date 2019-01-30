762d5c5558cf6de27b71e8fad33a10b5106fecae
DamianM <damian.michalski@kramp.com>
2019-01-30 09:21:25 +0100

    upload plików

--- modules/handlers.js
+++ modules/handlers.js
@@ -5,6 +5,7 @@ exports.upload = function(request, response) {
   console.log("Rozpoczynam obsługę żądania upload.");
   var form = new formidable.IncomingForm();
   form.parse(request, function(error, fields, files) {
+    if (error) throw error;
     fs.renameSync(files.upload.path, "test.png");
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("received image:<br/>");
@@ -15,7 +16,8 @@ exports.upload = function(request, response) {
 
 exports.welcome = function(request, response) {
   console.log("Rozpoczynam obsługę żądania welcome.");
-  fs.readFile('./templates/start.html', function(err, html) {
+  fs.readFile('./templates/start.html', function(error, html) {
+    if (error) throw error;
     response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
     response.write(html);
     response.end();
@@ -30,6 +32,7 @@ exports.error = function(request, response) {
 
 exports.show = function(request, response) {
   fs.readFile("test.png", "binary", function(error, file) {
+    if (error) throw error;
     response.writeHead(200, {"Content-Type": "image/png"});
     response.write(file, "binary");
     response.end();
